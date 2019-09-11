3267c5105493a5b51da3bb013ed7bd36df769814
Jayowen323542 <323542@student.mboutrecht.nl>
2019-09-11 13:33:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-X
+<?php ob_start(); session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Rent A Student</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom css -->
+
+</head>
+
+<body>
+
+    <main class="container">
+    <p>Halo Wereld</p>
+    </main>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
