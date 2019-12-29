414e9b4333cc8a43aed15c65b1ef22da0876f405
Nuha Camara <321592@student.mboutrecht.nl>
2019-12-29 15:44:15 +0100

    Update student_home.php

--- student_home.php
+++ student_home.php
@@ -1,75 +1,35 @@
-
-
 <div class="row">
-<div class="col-sm-3">
-<div class="card" style="width: 18rem;">
-  <img src="./img/Huis-schoonmaken.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Tijdelijke Schoonmaker Gezocht</h5>
-    <p class="card-text">Wij zoeken naaar een tijdelijke schoonmaker.</p>
+
+<?php
+$result = mysqli_query($conn,"SELECT * FROM `opdrachten` WHERE `idopdracht`"); 
+while($row = mysqli_fetch_assoc($result)){
+echo "<div class='col-sm-3'>
+<div class='card' style='width: 18rem;'>
+  <img src='./img/Huis-schoonmaken.jpg' class='card-img-top' alt='...'>
+  <div class='card-body'>
+    <h5 class='card-title'> ". $row['titel'] ."</h5>
+    <p class='card-text'>". $row['beschrijving'] ."</p>
   </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Datum: Van 12-1-2022 en dan elke dinsdag voor 3 weken</li>
-    <li class="list-group-item">Tijd: 3 uur per week</li>
-    <li class="list-group-item">Loon: 7 euro per uur</li>
-    <li class="list-group-item">
-    <p class="card-text">Gepost: 11/11/2021</p>
-    <p class="card-text">Geupdate: 12/12/2021</p>
+  <ul class='list-group list-group-flus'>
+    <li class='list-group-item'>Loon: ". $row['loon'] ."</li>
+    <li class='list-group-item'>Datum: ". $row['datum'] ."</li>
+    <li class='list-group-item'>
+    <p class='card-text'>Aangemaakt: ". $row['createDate'] ."</p>
+    <p class='card-text'>Geupdate: ". $row['updateDate'] ."</p>
     </li>
   </ul>
-  <div class="card-body">
-  <a href="./index.php?content=student_home" class="card-link">Soliciteer</a>
+  <div class='card-body'>
+  <a href='./index.php?content=opdrachten_reacties&id=". $row["idopdracht"] ."' class='card-link'>accepteer</a>
   </div>
 </div>
-</div>
+</div>";
 
-<div class="col-sm-3">
-<div class="card" style="width: 18rem;">
-  <img src="./img/Huis-schoonmaken.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Tijdelijke Schoonmaker Gezocht</h5>
-    <p class="card-text">Wij zoeken naaar een tijdelijke schoonmaker.</p>
+
+
+}
+
+  ?>
   </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Datum: Van 12-1-2022 en dan elke dinsdag voor 3 weken</li>
-    <li class="list-group-item">Tijd: 3 uur per week</li>
-    <li class="list-group-item">Loon: 7 euro per uur</li>
-    <li class="list-group-item">
-    <p class="card-text">Gepost: 11/11/2021</p>
-    <p class="card-text">Geupdate: 12/12/2021</p>
-    </li>
-  </ul>
-  <div class="card-body">
-  <a href="./index.php?content=student_home" class="card-link">Soliciteer</a>
-  </div>
-</div>
-</div>
-
-<div class="col-sm-3">
-<div class="card" style="width: 18rem;">
-  <img src="./img/Huis-schoonmaken.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Tijdelijke Schoonmaker Gezocht</h5>
-    <p class="card-text">Wij zoeken naaar een tijdelijke schoonmaker.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Datum: Van 12-1-2022 en dan elke dinsdag voor 3 weken</li>
-    <li class="list-group-item">Tijd: 3 uur per week</li>
-    <li class="list-group-item">Loon: 7 euro per uur</li>
-    <li class="list-group-item">
-    <p class="card-text">Gepost: 11/11/2021</p>
-    <p class="card-text">Geupdate: 12/12/2021</p>
-    </li>
-  </ul>
-  <div class="card-body">
-  <a href="./index.php?content=student_home" class="card-link">Soliciteer</a>
-  </div>
-</div>
-</div>
-
-</div>
-
-
 
 
 <br>
